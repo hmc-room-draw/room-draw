a786b7bc38eadf9850056f1569acf61955300d64
Sam Gearou <samgearou@gmail.com>
2017-10-09 21:09:53 -0700

    first mockup of login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,8 @@
 Rails.application.routes.draw do
-
-  get 'dorms/linde2'
-
-  get 'dorms/linde'
-
-  get 'dorms/atwood3'
-
-  get 'dorms/atwood2'
-
-  get 'dorms/atwood'
-
-  get 'dorms/case'
-
-  get 'dorms/case2'
-
-  get 'dorms/east2'
-
-  get 'dorms/west2'
-
-  get 'dorms/west'
-
-  get 'dorms/east'
-
-  get 'dorms/south2'
-
-  get 'dorms/south'
-
-  get 'dorms/north2'
-
-  get 'dorms/north'
-  get 'login/show'
-
-  resources :pulls
-  resources :room_assignments
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-
-  get    '/login',   to: 'sessions#new'
-  delete '/logout',  to: 'sessions#destroy'
-
-  resources :sessions, only: [:create, :destroy]
-
-  resources :users
-  resources :dorms
-  resources :rooms
-  resources :suites
-  resources :students
-
-  root "sessions#new"
- end
+	root 'static_pages#home'
+    get  '/help',    to: 'static_pages#help'
+    get  '/about',   to: 'static_pages#about'
+    get  '/contact', to: 'static_pages#contact'
+    get  '/signup',  to: 'users#new'
+    resources :users
+end
