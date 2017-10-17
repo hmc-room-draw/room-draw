ff73c610215dcd8bb741a5c689bc72a0662a1c5a
casejaz <jizhu@g.hmc.edu>
2017-10-17 16:37:15 -0700

    Clear code

--- app/controllers/adminpage_controller.rb
+++ app/controllers/adminpage_controller.rb
@@ -1,4 +1,6 @@
 class AdminpageController < ApplicationController
+
+  
   def reminder()
   end
 
@@ -17,9 +19,9 @@ class AdminpageController < ApplicationController
     end
 
     # Run at scheduled time
-    time = @schedule_time["send"]
     puts "NOOOO"
-    puts time
+    date = params
+    puts date
     User.all.each do |user|
       GeneralMailer.delay(queue:"reminder", run_at: 3.minutes.from_now).reminder_email(user)
     end
@@ -36,71 +38,4 @@ class AdminpageController < ApplicationController
     # end
 
   end
-
-
-  #Src: http://imnithin.in/asynchronously_synchronous.html
-  #https://apidock.com/rails/ActionController/Streaming/send_data
-
-# NOTE: This is solely used to test the csv-downloading functionality
-# We probably don't need this function in the actual app, and if we do,
-# then we'd have to replace users with students, and replace user.name with
-# user.first_namd and user.last_name
-  def download_users()
-    user_csv = CSV.generate do |csv|
-      csv << ["Name", "Email"] #Header
-      User.all.each do |user|
-        csv << [user.name, user.email]
-      end
-    end
-    send_data user_csv,
-      :type => 'text/csv',
-      :filename => 'users.csv',
-      :disposition => 'attachment'
-  end
-
-# NOTE: Untested - should be tested once DB is implemented.
-# Downloads a list of the final placements of all students.
-  def download_final_placements()
-    placements_csv = CSV.generate do |csv|
-      csv << ["First", "Last", "Class", "Number", "Dorm", "Room"]
-      Student.all.each do |student|
-        if student.room
-          user = student.user
-          room = student.room
-          dorm = student.room.dorm.name #TODO - check if dorm name is being stored
-          csv << [user.first_name,
-                  user.last_name,
-                  student.class,
-                  student.room_draw_number,
-                  dorm,
-                  room.room_num]
-        end
-      end
-    end
-    send_data placements_csv,
-      :type => 'text/csv',
-      :filename => 'placements.csv',
-      :disposition => 'attachment'
-  end
-
-  # Download a list of everyone who didn't participate in room draw.
-  def download_non_participants()
-    non_participants_csv = CSV.generate do |csv|
-      csv << ["First", "Last", "ID", "Class", "Number", "Email"]
-      Student.where(has_participated: false).find_each do |student|
-        user = Student.user
-        csv << [user.first_name,
-                user.last_name,
-                student.id,
-                student.class,
-                student.room_draw_number,
-                user.email]
-      end
-    end
-    send_data non_participants_csv,
-      :type => 'text/csv',
-      :filename => 'non_participants.csv',
-      :disposition => 'attachment'
-  end
-
 end
