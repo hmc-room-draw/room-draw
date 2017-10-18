d949c96a283db240172d608e6281ee0e999ab12b
Hyobin You <hyobinyou@gmail.com>
2017-10-18 03:50:58 -0700

    included pundit gem

---
+++ app/policies.rb
@@ -0,0 +1,12 @@
+class PostPolicy
+  attr_reader :user, :post
+
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
+
+  def update?
+    user.admin? or not post.published?
+  end
+end
