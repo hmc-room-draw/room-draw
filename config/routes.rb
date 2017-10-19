f3aafab91a913165dbd9f129b51e4bc5a4deda3f
Spencer Michaels <sxmichaels@gmail.com>
2017-10-19 11:41:49 -0700

    Add form redirection.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
 
+  get 'login/show'
+
   resources :pulls
   resources :room_assignments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get    '/login',   to: 'sessions#new'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :sessions, only: [:create, :destroy]
-  resource :login, only: [:show]
 
   resources :users
   resources :dorms
   resources :rooms
   resources :suites
 
-  root to: "login#show"
+  root "sessions#new"
 end
