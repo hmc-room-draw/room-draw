e28f83264deae05fd898124c177c50dc2f5350b6
Spencer Michaels <sxmichaels@gmail.com>
2017-10-22 17:43:02 -0700

    Fixed screwed-up Student object

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -69,6 +69,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:class, :room_draw_number, :has_participated)
+      params.require(:student).permit(:class_rank, :room_draw_number, :has_participated)
     end
 end
--- app/views/students/_student.json.jbuilder
+++ app/views/students/_student.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! student, :id, :class, :room_draw_number, :has_participated, :created_at, :updated_at
+json.extract! student, :id, :class_rank, :room_draw_number, :has_participated, :created_at, :updated_at
 json.url student_url(student, format: :json)
