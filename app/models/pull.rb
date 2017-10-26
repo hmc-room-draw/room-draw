27cd6d8188b7664407f111b94e8466de9fd1b746
Spencer Michaels <sxmichaels@gmail.com>
2017-10-26 14:13:10 -0700

    Update pull checking (not tested)

--- app/models/pull.rb
+++ app/models/pull.rb
@@ -3,34 +3,18 @@ class Pull < ApplicationRecord
   has_many :students, through: :room_assignments
   belongs_to :student
 
-  # Checks if pull1 can overwrite pull2
-  # @param pull1, pull2
-  #			IDs of pulls to be checked	
-  def self.check(pull1_id, pull2_id)
-  	puts "heyyo"
-  	puts pull1_id
-  	puts pull2_id
+  # Checks if self can overwrite another pull
+  # @param other_pull
+  #			Pull to be checked
+  def can_override(other_pull)
+  	return self.student.room_draw_number > other_pull.student.room_draw_number
+  end
 
-  	@pull1 = Pull.find(pull1_id)
-  	@pull2 = Pull.find(pull2_id)
-
-  	# checks if pull1 has a lower number than pull2
-  	if @pull1.student.room_draw_number < @pull2.student.room_draw_number
-  		puts "can overwrite"
-  		# check room assignments for a conflict
-  		@pull1.room_assignments.each do |x|
-  			puts x.room.number
-  			@pull2.room_assignments.each do |y|
-  				puts y.room.number
-  				if x.room.number == y.room.number
-  					puts "room number conflict!"
-  					return true
-  				end
-  			end
-  		end
-  	end
-
-  	return false
+  # Returns a list of Pulls conflicting with this one
+  def get_conflicting_pulls
+    room_ids = self.room_assignments.map{ |ra| ra.room_id }
+    conflicting_assignments = RoomAssignment.where(room_id: room_ids)
+    conflicting_assignments.map{ |asn| asn.pull }
   end
 
 end
