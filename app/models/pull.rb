45be60959af68d5ab3ed9fbb518c25d1b1e09d65
pmcdonough35 <pmcdonough35@gmail.com>
2017-10-27 10:20:18 -0700

    made spencers  pr revisions

--- app/models/pull.rb
+++ app/models/pull.rb
@@ -1,7 +1,5 @@
 class Pull < ApplicationRecord
 
-  validates :message, presence: true
-
   has_many :room_assignments
   has_many :students, through: :room_assignments
   belongs_to :student
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,9 @@ class Room < ApplicationRecord
 
   validates :capacity, presence: true
   validates :floor, presence: true
+  validates :floor, :numericality => { :greater_than_or_equal_to => 0 }
   validates :number, presence: true
+  validates :number, :numericality => { :greater_than_or_equal_to => 0 }
 
   belongs_to :dorm
   belongs_to :suite
