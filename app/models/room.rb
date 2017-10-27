f882aa705ae96ffb4d599b5bc95b6a4b66f1cb91
pmcdonough35 <pmcdonough35@gmail.com>
2017-10-27 10:34:57 -0700

    fixed room.rb validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,8 @@
 class Room < ApplicationRecord
 
   validates :capacity, presence: true
-  validates :floor, presence: true
-  validates :floor, :numericality => { :greater_than_or_equal_to => 0 }
-  validates :number, presence: true
-  validates :number, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :floor, presence: true, numericality => { :greater_than_or_equal_to => 0 }
+  validates :number, presence: true, numericality => { :greater_than_or_equal_to => 0 }
 
   belongs_to :dorm
   belongs_to :suite
