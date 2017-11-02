d17d18abc4e5997e18d8352546bf49163a4b2584
Hyobin You <hyobinyou@gmail.com>
2017-11-02 14:15:28 -0700

    changed view

--- app/helpers/room_assignments_helper.rb
+++ app/helpers/room_assignments_helper.rb
@@ -1,2 +1,10 @@
 module RoomAssignmentsHelper
+    def student_name(room_assignment)
+        student = Student.find(room_assignment.student_id)
+        @student_name = User.find(student.user_id).pluck(:first_name, :last_name)
+    end
+
+    def student_number(pull)
+        @student_number = Student.find(room_assignment.student_id).pluck(:class_rank, :room_draw_number)
+    end
 end
