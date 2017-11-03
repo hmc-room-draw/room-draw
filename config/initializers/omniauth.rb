27f02e3531607b9aa3660ef76b899df050a0594c
Annalise Ko <annaliseliuko@gmail.com>
2017-11-02 22:33:35 -0700

    radio buttons added to pull request form

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, "810840116642-c3snbc808k3ugma6st4oln3dvk84rr93.apps.googleusercontent.com", "XhnAjQsjl6U8iR7Nmnn2vJOm"
 end
 
 Rails.application.config.form_url = ENV['FORM_URL']
