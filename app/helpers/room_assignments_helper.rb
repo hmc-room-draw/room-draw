c9d83eb026d3209b2da5b585797e8c78a8817f1a
Hyobin You <hyobinyou@gmail.com>
2017-11-05 16:37:14 -0800

    remove comments

--- app/helpers/room_assignments_helper.rb
+++ app/helpers/room_assignments_helper.rb
@@ -9,13 +9,4 @@ module RoomAssignmentsHelper
         @student_number = Student.find(room_assignment.student_id).pluck(:class_rank, :room_draw_number)
     end
 
-    def room_name(room_assignment)
-    	@room_name = room_assignment.room.number
-    	/room = Room.find(room_assignment.room_id)
-    	suite = Suite.find(room.suite_id)
-    	dorm = Dorm.find(room.dorm_id)
-    	room_number = room.number
-    	room_floor = room.floor
-    	@room_name = dorm + suite + room_floor + room_number/
-    end
 end
