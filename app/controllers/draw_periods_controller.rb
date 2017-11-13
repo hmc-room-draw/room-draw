64401bb664db10fb39517f5323664ce7ef865997
elfran <elfranks@hmc.edu>
2017-11-13 12:55:47 -0800

    adding landing page

--- app/controllers/draw_periods_controller.rb
+++ app/controllers/draw_periods_controller.rb
@@ -50,6 +50,26 @@ class DrawPeriodsController < ApplicationController
             format.html { redirect_to draw_periods_url, notice: 'Draw Period was successfully canceled.' }
             format.json { head :no_content }
         end
+    end 
+
+    #TODO
+    def uploadRoster
+        render html: "<script>alert('Upload Roster Called')</script>".html_safe
+    end
+    
+    #TODO
+    def downloadStudents
+        render html: "<script>alert('Download Students Called')</script>".html_safe
+    end
+    
+    #TODO
+    def downloadPulls
+        render html: "<script>alert('Download Pulls Called')</script>".html_safe
+    end
+    
+    #TODO
+    def setStartEndDate
+        render html: "<script>alert('setStartEndDate Called')</script>".html_safe
     end
 
     private
