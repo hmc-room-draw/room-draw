46a81dc6f7b644ef25ba39e0b58373fd1c15ad01
Hyobin You <hyobinyou@gmail.com>
2017-11-13 10:59:10 -0800

    nested forms

--- app/models/pull.rb
+++ app/models/pull.rb
@@ -2,7 +2,8 @@ class Pull < ApplicationRecord
   has_many :room_assignments
   has_many :students, through: :room_assignments
   accepts_nested_attributes_for :room_assignments
-  
+  #TODO: add rejection parameters for :room_assignments
+
   belongs_to :student
 
   #TO DO: ensure students are not in another pull
