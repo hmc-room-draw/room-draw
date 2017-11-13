d4fec0c050394e21051ade36e78a88b5a2484cd1
AliengirlLiv <aliengirl@google.com>
2017-11-12 22:24:42 -0800

    Send delayed emails

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -2,7 +2,7 @@ class GeneralMailer < ApplicationMailer
 
   def reminder_email(user, subject, content)
     @user = user
-    mail(to: "jizhu@g.hmc.edu", subject: subject, body: content)
+    mail(to: user.email, subject: subject, body: content)
   end
 
 end
