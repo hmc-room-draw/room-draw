40b557b035e368d04e54ff0429a533546b381602
Hyobin You <hyobinyou@gmail.com>
2017-11-12 22:37:49 -0800

    view change

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,5 +1,5 @@
 module StudentsHelper
     def get_name(student)
-        @name = student.user.first_name + " " + student.user.last_name
-      end
+    	@name = student.user.first_name + " " + student.user.last_name
+    end
 end
