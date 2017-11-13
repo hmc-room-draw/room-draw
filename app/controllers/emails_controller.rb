78a371f3504b47d1e897f5fdc8901138c2a37c2a
AliengirlLiv <aliengirl@google.com>
2017-11-12 20:18:48 -0800

    Clean up emails controller

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -30,33 +30,18 @@ class EmailsController < ApplicationController
 
     # Run bin/delayed_job start to process all jobs
 
-    # delayed job
-
     diffM = (send_date-DateTime.now)
-    puts DateTime.now, send_date
     hours=send_date.minus_with_duration(DateTime.now)
-    puts hours
     user = User.all.first
-    puts user
     Student.all.each do |student|
       if student.has_participated == false
-        puts student.class_rank
         message = GeneralMailer.reminder_email(user, subject, content)
-        puts message
         message.deliver_now
         if student.user
           user = student.user
-          puts user
         end 
       end
     end
-    # User.all.each do |user|
-    #   GeneralMailer.reminder_email(user).deliver_later
-    # end
-    # # send at scheduled date time by admin
-    # User.all.each do |user|
-    #   GeneralMailer.delay(queue:"reminder", run_at: 10.minutes.from_now).reminder_email(user)
-    # end
   end
 
   def destory
