2bb5b3a2b0ea65c97fcb74a61a40dedcaddfe90f
Annalise Ko <annaliseliuko@gmail.com>
2017-11-15 17:00:02 -0800

    new pull form

--- app/controllers/dorms_controller.rb
+++ app/controllers/dorms_controller.rb
@@ -11,6 +11,12 @@ class DormsController < ApplicationController
   # GET /dorms/1.json
   def show
       @rooms = @dorm.room
+      @pull = Pull.new
+      3.times {@pull.room_assignments.build}
+      #TODO: Get only the necessary information
+      @students = Student.all
+      @rooms = Room.all
+      @dorms = Dorm.all
   end
 
   # GET /dorms/new
@@ -20,6 +26,9 @@ class DormsController < ApplicationController
 
   # GET /dorms/1/edit
   def edit
+      # @students = Student.all
+      # @rooms = Room.all
+      # @dorms = Dorm.all
   end
 
   # POST /dorms
@@ -71,5 +80,15 @@ class DormsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def dorm_params
       params.require(:dorm).permit(:name)
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pull
+      @pull = Pull.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pull_params
+      params.require(:pull).permit(:message, :student_id, :round, room_assignments_attributes: [:assignment_type, :student_id, :pull_id, :room_id])
     end
 end
