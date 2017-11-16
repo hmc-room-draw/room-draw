7243d9da67b5eb7e47d95bc7d1ccd5d61af5b788
Annalise Ko <annaliseliuko@gmail.com>
2017-11-15 17:03:53 -0800

    attributes in pull.rb

--- app/models/pull.rb
+++ app/models/pull.rb
@@ -1,5 +1,8 @@
 class Pull < ApplicationRecord
   has_many :room_assignments
   has_many :students, through: :room_assignments
+
+  accepts_nested_attributes_for :room_assignments
+
   belongs_to :student
 end
