c14d08854e7d80191ad620b438105ee4fafb882f
AliengirlLiv <aliengirl@google.com>
2017-11-18 12:59:39 -0800

    merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20171116071921) do
 
   create_table "pulls", force: :cascade do |t|
     t.string "message"
-    t.integer "round"
     t.integer "student_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -111,7 +110,6 @@ ActiveRecord::Schema.define(version: 20171116071921) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
