cb93e5f1061772ee1e560641ba88f87711b72e47
g_rocket <g.rocket1@gmail.com>
2017-11-19 04:51:21 -0800

    fix duplicate confirmation dialog

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require bootstrap-sprockets
 //
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap
 //= require bootstrap-sprockets
 //= require moment
