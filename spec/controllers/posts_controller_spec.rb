0674190045ba1135c2aabc0823c47c34d272616e
Spencer Michaels <sxmichaels@gmail.com>
2017-11-18 23:03:24 -0800

    Fix typos + refactor email controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -30,9 +30,9 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe "GET #destory" do
+  describe "GET #destroy" do
     it "returns http success" do
-      get :destory
+      get :destroy
       expect(response).to have_http_status(:success)
     end
   end
