2220f1db63bc0235d3d89414a0889eece14527e5
jesswinssinger <jessica@winssinger.com>
2017-11-22 14:38:52 -0700

    Redirect admin to admin/home upon login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,22 @@ class SessionsController < ApplicationController
   #skip_before_action :check_login, :check_form
 
   def create
-    user = User.from_omniauth(request.env['omniauth.auth'])
-    if user
-      session[:user_id] = user.id
-      redirect_to dorms_path
-    else
-      flash[:alert] = "No account exists with the given email."
-      redirect_to root_path
-    end
+      user = User.from_omniauth(request.env['omniauth.auth'])
+      if user
+          session[:user_id] = user.id
+          if user.is_admin
+              redirect_to admin_home_path
+          else
+              redirect_to dorms_path
+          end
+      else
+          flash[:alert] = "No account exists with the given email."
+          redirect_to root_path
+      end
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+      session[:user_id] = nil
+      redirect_to root_path
   end
 end
