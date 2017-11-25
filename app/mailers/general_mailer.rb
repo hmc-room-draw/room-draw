57d36c873cab6e517e9062295ba80addd1f73e2d
AliengirlLiv <aliengirl@google.com>
2017-11-25 12:32:24 -0800

    Minor changes, nothing important

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -10,47 +10,43 @@ class GeneralMailer < ApplicationMailer
   end
 
   def scheduled_email(emailId)
-    #mail(to: 'owatkins@hmc.edu', subject: "GOT TO THE FUNCTION!", body: "NOTHING INTERESTING.")
     email = Email.find emailId
     if email
-      # Student.all.each do |student|
-      #   send_mail = false
-      #   case student.status
-      #   when :never_logged_in
-      #     if email.send_to_never_logged_in
-      #       send_mail = true
-      #     end
-      #   when :never_pulled_room
-      #     if email.send_to_never_pulled_room
-      #       send_mail = true
-      #     end
-      #   when :formerly_in_room
-      #     if email.send_to_formerly_in_room
-      #       send_mail = true
-      #     end
-      #   when :in_room
-      #     if email.send_to_in_room
-      #       send_mail = true
-      #     end
-      #   else
-      #     send_mail = false
-      #   end 
+      Student.all.each do |student|
+        send_mail = false
+        case student.status
+        when :never_logged_in
+          if email.send_to_never_logged_in
+            send_mail = true
+          end
+        when :never_pulled_room
+          if email.send_to_never_pulled_room
+            send_mail = true
+          end
+        when :formerly_in_room
+          if email.send_to_formerly_in_room
+            send_mail = true
+          end
+        when :in_room
+          if email.send_to_in_room
+            send_mail = true
+          end
+        else
+          send_mail = false
+        end 
 
-      #   if send_mail
-      #     mail(to: 'oliviagracewatkins@gmail.com', subject: student.user.first_name + student.user.last_name, body: email.description)
-      #     #mail(to: student.user.email, subject: email.subject, body: email.description)
-      #   end
-      # end
+        if send_mail
+          mail(to: student.user.email, subject: email.subject, body: email.description)
+        end
+      end
       if email.send_to_admins
         User.all.each do |user|
           if user.is_admin
-            #mail(to: 'oliviagracewatkins@gmail.com', subject: user.first_name + user.last_name, body: email.description)
             mail(to: user.email, subject: email.subject, body: email.description)
           end
         end 
       end
       email.update(sent_status: true)
-      #mail(to: 'owatkins@g.hmc.edu', subject: "UPDATED!", body: email.sent_status ? "SENT" : "UNSENT")
     end
   end
 
