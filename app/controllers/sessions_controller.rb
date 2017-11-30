b527fcb824de40b13de079f939a30c2ddfe9cab1
elfran <elfranks@hmc.edu>
2017-11-29 19:20:45 -0800

    minor fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user
       session[:user_id] = user.id
-      if(user.is_admin)
+      if user.is_admin
         redirect_to admin_home_path
       else
         redirect_to dorms_path
