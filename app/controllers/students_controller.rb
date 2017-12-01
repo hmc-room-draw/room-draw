19b0cc1e2f89658b0a6e4b838caca0d175899bea
jesswinssinger <jessica@winssinger.com>
2017-11-30 22:42:51 -0800

    Refactoring, fix destroy action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,10 +6,8 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-
   def show
   end
-
 
   def new
     @user = User.new
@@ -18,48 +16,37 @@ class StudentsController < ApplicationController
     @method = :post
   end
 
-
   def edit
     @action = "update"
     @method = :patch
   end
 
-
   def create
     @user = User.new(user_params)
     @student = Student.new(student_params)
 
-    # check if a user already exists for that email. if yes update student for it
-    if @existing_user = User.where(email: @user.email).first
-      # if the user already has a student can't override
-      if @existing_user.student
-        respond_to do |format|
-            format.html { redirect_to @existing_user.student, notice: 'There is already a user with that email adress' }
-        end
-      # if user has no student create one
-      else
-        respond_to do |format|
-        @existing_user.student = @student
-        if @existing_user.save && @existing_user.student.save
-            format.html { redirect_to @existing_user.student, notice: 'Student was added to existing user.' }
+    respond_to do |format|
+      if @existing_user = User.where(email: @user.email).first
+        if @existing_user.student
+          format.html { redirect_to @existing_user.student, alert: 'There is already a user with that email address.' }
         else
-          format.html { render :edit }
+          @existing_user.student = @student
+          if @existing_user.save && @existing_user.student.save
+              format.html { redirect_to @existing_user.student, notice: 'Student was added to existing user.' }
+          else
+            format.html { render :edit }
+          end
         end
-      end
-    end
-    # if no user exists create one
-    else
-      @user.student = @student
-      respond_to do |format|
+      else
+        @user.student = @student
         if @user.save && @user.student.save
           format.html { redirect_to @user.student, notice: 'Student was successfully created.' }
         else
           format.html { render :new }
         end
-      end
-    end 
+      end 
+    end
   end
-
 
   def update    
     respond_to do |format|
@@ -71,10 +58,8 @@ class StudentsController < ApplicationController
     end
   end
 
-
   def destroy
     @user.destroy
-    @student.destroy
 
     respond_to do |format|
       if current_user.is_admin 
@@ -96,7 +81,6 @@ class StudentsController < ApplicationController
       params.fetch(:user).permit(:first_name, :last_name, :email, :is_admin)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
       params[:user].fetch(:student).permit(:class_rank, :room_draw_number, :has_participated, :user_id, :has_completed_form, :number_is_last)
     end
