6711ef826dec94b4bcfdb8fe2d94d762ed5e0bea
jesswinssinger <jessica@winssinger.com>
2017-12-02 15:18:11 -0800

    Styling

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,8 +1,8 @@
 class EmailsController < ApplicationController
   before_action :set_email, only: [:receiver, :edit, :update, :destroy]
   after_action :verify_authorized
-  helper_method :get_person_counts, :recipients
-
+  include EmailsHelper
+  
   def new
     authorize Email
     @email = Email.new
@@ -65,42 +65,6 @@ class EmailsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to emails_path, notice: 'Email was successfully destroyed.' }
       format.json { head :no_content }
-    end
-  end
-
-  def recipients(email)
-    @recipients = ""
-    if email.send_to_never_logged_in
-      @recipients += "Never logged in<br>"
-    end
-    if email.send_to_never_pulled_room
-      @recipients += "Never pulled room<br>"
-    end
-    if email.send_to_formerly_in_room
-      @recipients += "Formerly in room<br>"
-    end
-    if email.send_to_in_room
-      @recipients += "In room<br>"
-    end
-    if email.send_to_admins
-      @recipients += "Admins"
-    end
-    return @recipients
-  end
-
-  def get_person_counts(status_type)
-    authorize Email
-    count = 0
-    case status_type
-    when "admin"
-      count = User.all.select{|user| user.is_admin == true}.length
-    else
-      count = Student.all.select{|student| student.status == status_type}.length
-    end
-    if count == 1
-      return " (currently 1 person)"
-    else
-      return " (currently #{count} people)"
     end
   end
 
--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -1,2 +1,44 @@
 module EmailsHelper
+  def recipients(email)
+    @recipients = ""
+    if email.send_to_never_logged_in
+      @recipients += "Never logged in<br>"
+    end
+    if email.send_to_never_pulled_room
+      @recipients += "Never pulled room<br>"
+    end
+    if email.send_to_formerly_in_room
+      @recipients += "Formerly in room<br>"
+    end
+    if email.send_to_in_room
+      @recipients += "In room<br>"
+    end
+    if email.send_to_admins
+      @recipients += "Admins"
+    end
+    return @recipients
+  end
+
+  def get_person_counts(status_type)
+    authorize Email
+    count = 0
+    case status_type
+    when "admin"
+      count = User.all.select{|user| user.is_admin == true}.length
+    else
+      count = Student.all.select{|student| student.status == status_type}.length
+    end
+    if count == 1
+      return " (currently 1 person)"
+    else
+      return " (currently #{count} people)"
+    end
+  end
+
+  def destroy_action(email)
+    if email.sent_status
+      return "Delete"
+    end
+    return "Cancel"
+  end
 end
