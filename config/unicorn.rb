fdb68698df0aaf523c31c8374c6173e8fdb71702
AliengirlLiv <oliviagracewatkins@gmail.com>
2018-03-13 01:52:00 +0000

    Changes for new aws instance

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,27 @@
-working_directory File.expand_path("../..", __FILE__)
-worker_processes 5
-listen "/tmp/unicorn.sock"
+#working_directory File.expand_path("../..", __FILE__)
+#worker_processes 5
+#listen "/tmp/unicorn.sock"
+#timeout 30
+#pid "/tmp/unicorn_roomdraw.pid"
+#stdout_path "/tmp/unicorn.log"
+#stderr_path "/tmp/unicorn.log"
+# set path to application
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+working_directory app_dir
+
+
+# Set unicorn options
+worker_processes 2
+preload_app true
 timeout 30
-pid "/tmp/unicorn_roomdraw.pid"
-stdout_path "/tmp/unicorn.log"
-stderr_path "/tmp/unicorn.log"
+
+# Set up socket location
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{shared_dir}/pids/unicorn.pid"
