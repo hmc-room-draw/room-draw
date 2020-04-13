ae4f512d570c2242e4a18e7fb79e98d13350078b
athli <33944524+athli@users.noreply.github.com>
2020-04-12 20:11:54 -0700

    Update urls.rb
    stupid fucking bug

--- config/initializers/urls.rb
+++ config/initializers/urls.rb
@@ -1,5 +1,5 @@
 require './lib/google_api/drive.rb'
-
+=begin
 form_url = ENV['FORM_URL']
 Rails.application.config.form_url = form_url
 
@@ -8,7 +8,7 @@ responses_url = ENV['RESPONSES_URL']
 # Extracts the document key from a form URL formatted like:
 # https://docs.google.com/spreadsheets/d/1i0lcWJqCu9Jk6VPb3xvrJnNkMtw__hne9WrN6O8gDq4/
 KEY_REGEX = /.+\/d\/(.+)\/.*/
-=begin
+
 match = KEY_REGEX.match(responses_url)
 raise "Malformed response spreadsheet URL `#{responses_url}`. Expected format https://docs.google.com/spreadsheets/d/<KEY>/..." unless match
 
