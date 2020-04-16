681d90391348d202898d3ae800dcbf059c2b8239
bbk001 <benjaminbrookskhoury@gmail.com>
2020-04-15 21:02:33 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -229,7 +229,7 @@ Room.create(floor: 2, capacity: 2, number: '252', dorm_id: 2)
 Room.create(floor: 2, capacity: 2, number: '253', dorm_id: 2)
 Room.create(floor: 2, capacity: 2, number: '254', dorm_id: 2)
 Room.create(floor: 2, capacity: 3, number: '255', dorm_id: 2)
-Room.create(floor: 2, capacity: 3, number: '256', dorm_id: 2)
+Room.create(floor: 2, capacity: 4, number: '256', dorm_id: 2)
 Room.create(floor: 2, capacity: 4, number: '257', dorm_id: 2)
 Room.create(floor: 2, capacity: 4, number: '258', dorm_id: 2)
 
